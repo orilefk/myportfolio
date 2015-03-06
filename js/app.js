abc1d518a77018e3a68da79539d007507a1d34e2
orilefk <olefkovitz@mac-mbp2013l.local>
2015-03-06 09:30:33 -0500

    js addition

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	var aboutTopPosition = $('.aboutpage').offset().top;
+	var projectsTopPosition = $('.projectspage').offset().top;
+	var contactTopPosition = $('.social').offset().top;
+	var homeTopPosition = $('.introduction').offset().top;
+
+	$('#about').click(function(){
+		$('html, body').animate({scrollTop:aboutTopPosition}, 'slow');
+		return false;
+	});
+	$('#projects').click(function(){
+		$('html, body').animate({scrollTop:projectsTopPosition}, 'slow');
+		return false;
+	});
+	$('#contact').click(function(){
+		$('html, body').animate({scrollTop:contactTopPosition}, 'slow');
+		return false;
+	});
+	$('.logo').click(function(){
+		$('html, body').animate({scrollTop:homeTopPosition}, 'slow');
+		return false;
+	});
+});
